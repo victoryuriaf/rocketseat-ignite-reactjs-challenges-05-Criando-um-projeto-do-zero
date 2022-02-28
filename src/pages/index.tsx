ce21b0518d6a58990764c90c2a101aade86fcbc5
Victor Yuri <victoryuri.af@gmail.com>
2022-02-28 16:27:24 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import Header from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +28,84 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum título</strong>
+              <p>Pensando em sincronizacao em vez de ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Victor Yuri
+                </li>
+              </ul>
+            </a>
+          </Link>
 
-//   // TODO
-// };
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum título</strong>
+              <p>Pensando em sincronizacao em vez de ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Victor Yuri
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <button type="button">
+            Carregar mais posts
+          </button>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.Predicates.at('document.type', 'posts'),
+  ], {
+    pageSize: 1,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    }
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  }
+
+  return {
+    props: {
+      postsPagination,
+    }
+  }
+};
